cfb03b7bc54bf8f187515634d74d1e435e4d2d2c
shamkumbhar <shamkumbhar47@gmail.com>
2025-07-16 08:20:15 +0530

    Update DeiController.java

--- Backend_hrms/src/main/java/com/quantumsoft/hrms/controller/DeiController.java
+++ Backend_hrms/src/main/java/com/quantumsoft/hrms/controller/DeiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@CrossOrigin("*")
+//@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/dei")
 @RequiredArgsConstructor
