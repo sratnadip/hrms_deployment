efe3d45ee768a0e7ca777c9d7580bc90febb76b8
shamkumbhar <shamkumbhar47@gmail.com>
2025-07-16 08:24:15 +0530

    Update UserController.java

--- Backend_hrms/src/main/java/com/quantumsoft/hrms/controller/UserController.java
+++ Backend_hrms/src/main/java/com/quantumsoft/hrms/controller/UserController.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin("*")
+//@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/api/user")
 public class UserController {
